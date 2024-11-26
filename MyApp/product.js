ddc18488fbdad920df51c2eefbe5009a6eeca70e
RF-23 <ridafarheen321@gmail.com>
2024-11-26 16:25:07 +0400

    done

---
+++ MyApp/product.js
@@ -0,0 +1,46 @@
+let products=[
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of",
+        price: 2000,
+        image: "./images/image1.jpeg",
+        availableInventory: 10,
+        rating:3
+    },
+    {
+        id: 1002,
+        title: "Dog Food, 25lb bag",
+        description: "A 25 pound bag of",
+        price: 2000,
+        image: "./images/image.jpg",
+        availableInventory: 10,
+        rating:2
+    },
+    {
+        id: 103,
+        title: "Bird Food, 25lb bag",
+        description: "A 25 pound bag of",
+        price: 2000,
+        image: "./images/image1.jpeg",
+        availableInventory: 10,
+        rating:4
+    },
+    {
+        id: 1004,
+        title: "Fish Food, 25lb bag",
+        description: "A 25 pound bag of",
+        price: 2000,
+        image: "images/image1.jpeg",
+        availableInventory: 10,
+        rating:1
+    }, {
+        id: 1005,
+        title: "Hen Food, 25lb bag",
+        description: "A 25 pound bag of",
+        price: 2000,
+        image: "images/image1.jpeg",
+        availableInventory: 10,
+        rating:5
+    }
+]
